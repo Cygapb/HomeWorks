276b028ed37b21cacc3919ecf19791bdbc2d456e
Cygapb <sudarchenko@gmail.com>
2016-10-11 20:07:20 +0300

    Task 2. Not completed yet.

---
+++ src/ua/artcode/week1/Task2.java
@@ -0,0 +1,18 @@
+package ua.artcode.week1;
+
+import java.util.Scanner;
+
+/**
+ * Created by Администратор on 11.10.2016.
+ * 4.2. Пользователь вводит три числа с консоли, нужно вывести на консоль наибольшее, наименьшее
+ */
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int variable1 = sc.nextInt();
+        int variable2 = sc.nextInt();
+        int variable3 = sc.nextInt();
+        if ()
+    }
+}
