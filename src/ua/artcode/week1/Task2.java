0894faa66d4dac61e4ede9afcea42202ac854850
Cygapb <sudarchenko@gmail.com>
2016-10-12 14:08:54 +0300

    Task 2. Completed

--- src/ua/artcode/week1/Task2.java
+++ src/ua/artcode/week1/Task2.java
@@ -10,9 +10,22 @@ import java.util.Scanner;
 public class Task2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int variable1 = sc.nextInt();
-        int variable2 = sc.nextInt();
-        int variable3 = sc.nextInt();
-        if ()
+        System.out.println("Add values: ");
+        int v1 = sc.nextInt();
+        int v2 = sc.nextInt();
+        int v3 = sc.nextInt();
+            int max = v1;
+            int min = v3;
+        if (max > v2);
+        else max = v2;
+        if (max > v3);
+        else max = v3;
+        if (min < v1);
+        else min = v1;
+        if (min < v2);
+        else min = v2;
+        System.out.println("Maximum number " + max);
+        System.out.println("Minimum number " + min);
+
     }
 }
