c4df74e20e9a5208683f5fdf7a14c92c9ada1ee8
Cygapb <sudarchenko@gmail.com>
2016-10-19 12:36:19 +0300

    corrected some issues

--- src/ua/artcode/week1/Task1.java
+++ src/ua/artcode/week1/Task1.java
@@ -1,6 +1,12 @@
 package ua.artcode.week1;
 
 /**
+ * Created by Администратор on 19.10.2016.
+ */
+
+import java.util.*;
+
+/**
  * Created by Администратор on 11.10.2016.
  *4.1. Пользователь вводит параметр с консоли (текущий час от 0 до 24)
  Если время от 9 до 18, то выводим "Я на работе", в другом случае "Я отдыхаю")
@@ -10,15 +16,17 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner type = new Scanner(System.in);{
-            System.out.println("Pls check my schedule:");}
+            System.out.println("Pls check my schedule:");
+        }
         int time = type.nextInt();
         if (time >= 9 && time <= 18) {
-            System.out.println("I am on a my job");}
-        else if(time > 18 && time  <=24 ){
-            System.out.println("I am at home");}
-        else if (time > 24 && time < 1000) {
+            System.out.println("I am on a my job");
+        }
+        else if((time > 18 && time  <=24) || (time < 9 && time >= 1 )) {
+            System.out.println("I am at home");
+        }
+        else if (time > 24 && time < 100) {
             System.out.println("Wrong time.Sorry.");
         }
     }
 }
-
